07389e1651ae820c2133aefce33632e96e1110f7
Nicolás Muñoz <nicolasmunozd@gmail.com>
2023-03-16 20:40:52 -0300

    se agregaron los componentes Features y Feature

---
+++ src/components/Feature/index.tsx
@@ -0,0 +1,21 @@
+import './Feature.css';
+interface FeatureProps{
+    icon: string;
+    title: string;
+    text: string;
+}
+
+
+const Feature = ({ icon, title, text }:FeatureProps) =>{ 
+    return(
+        <div className="Feature">
+            <img src={icon}/>
+            <div>
+                <div className="Feature__title">{title}</div>
+                <div className="Feature__text">{text}</div>
+            </div>
+        </div>
+    );
+}
+
+export { Feature }
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import { Features } from "../components/Features";
 import { Hero } from "../components/Hero";
 
 const Home = ()=>{
     return (
         <React.Fragment>
             <Hero />
+            <Features />
             
         </React.Fragment>
         )
